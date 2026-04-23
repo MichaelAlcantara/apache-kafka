42b60c2601b8cf9cd4e4e71ab0a8c64b47259c9c
michael <michael.raposo@hotmail.com>
2026-04-23 17:22:54 -0300

    Usando função de Callback

--- str-producer/src/main/java/com/michaelAlcanatara/str_producer/services/StringProducerService.java
+++ str-producer/src/main/java/com/michaelAlcanatara/str_producer/services/StringProducerService.java
@@ -1,17 +1,28 @@
 package com.michaelAlcanatara.str_producer.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StringProducerService {
-	
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
-	
-	public void sendMessage(String message) {
-		kafkaTemplate.send("str-topic", message);
-	}
 
+	@Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    
+    private static final Logger log = LogManager.getLogger(StringProducerService.class);
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                	if(success != null) {
+                		log.info("Send message with success {}", message);
+                		log.info("Partition {}, Offset {}", success.getRecordMetadata().partition(), success.getRecordMetadata().offset());
+                	}
+                },
+                error -> log.error("Error send message")
+        );
+    }
 }
